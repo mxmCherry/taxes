7e7c1184e14b743bb579f6383d2d4ebf0eb59ea4
mxmCherry <mxmCherry@gmail.com>
2021-12-22 23:45:48 +0200

    tax: Round() comment - justify bad code

--- internal/tax/round.go
+++ internal/tax/round.go
@@ -4,6 +4,7 @@ import "math/big"
 
 // Round rounds provided float to `prec` number of decimals after comma.
 func Round(x *big.Float, prec uint) *big.Float {
+	// heavy, dirty, but does rounding job just fine
 	str := x.Text('f', int(prec)) // already rounded
 	res, _, err := big.ParseFloat(str, 10, 0, big.ToNearestEven)
 	if err != nil {
