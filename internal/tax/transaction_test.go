65e14ebe01de7b4e74034b94d4faef166d5ac864
mxmCherry <mxmCherry@gmail.com>
2021-12-23 19:02:12 +0200

    tax: impl Transaction.Validate tests

---
+++ internal/tax/transaction_test.go
@@ -0,0 +1,34 @@
+package tax_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	. "github.com/mxmCherry/taxes/v2/internal/tax"
+)
+
+var _ = Describe("Transaction", func() {
+	DescribeTable("Validate",
+		func(tx *Transaction, errStr string) {
+			if errStr == "" {
+				Expect(tx.Validate()).To(Succeed())
+			} else {
+				Expect(tx.Validate()).To(MatchError(errStr))
+			}
+		},
+
+		Entry("nil",
+			nil,
+			"cannot be nil",
+		),
+		Entry("bad amount",
+			&Transaction{Amount: 0.00, Currency: "UAH"},
+			"amount 0.000000 if invalid: should be greater than 0",
+		),
+		Entry("bad currency",
+			&Transaction{Amount: 0.01, Currency: "foo"},
+			`currency "foo" is invalid: should be a 3-letter ISO code, uppercased (like "UAH")`,
+		),
+	)
+})
